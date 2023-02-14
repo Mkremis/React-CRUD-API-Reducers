266623a665eed5890e62431bb2f6c55cc7fae3c3
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-02-13 23:16:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-CRUD-API-Reducers
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-mh6vne)
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CrudApi from './components/CrudApi';
+
+function App() {
+  return (
+    <>
+      <h1>CRUD API con Reducers</h1>
+
+      <CrudApi />
+    </>
+  );
+}
+
+export default App;
